79e19d070a34c3a6cf877d2afde5a894132aec2c
Nahide Gelgec <nahidegelgec@Nahides-MacBook-Pro.local>
2023-12-27 22:31:23 -0500

    changes

--- cypress/e2e/ticketCancellation.spec.cy.js
+++ cypress/e2e/ticketCancellation.spec.cy.js
@@ -69,3 +69,57 @@ describe("comboios de portugal suite", () => {
     });
   });
 });
+
+
+/*
+Please find the below points
+
+Oddities noticed from the website from a functional or UX perspective:
+
+1. Font in ticket submission flow has moderate UX.
+
+
+
+2. Duration format should be modified from 09h29 to 09h 29m.
+
+
+
+3. When we click on cancel button on options section page it navigated to home page - Is it expected?
+
+
+
+4. The "Submit" button is not even visible when it is in a disabled state.
+
+
+
+Changes to be made to the website for ease of implementation of a FE automation test suite:
+
+1. The cookies should be presented at the bottom part
+
+
+
+2. On the page where user purchase tickets the majority of the page is occupied by the headers, logo, and menu items
+we must adjust these alignments to provide the user to choose user actions from application.
+
+
+
+3. The homepage logo and header buttons are not properly aligned, there are extra spaces causing a misalignment.
+
+
+
+4. And, if we have the customized locators for each field it would be good for automation scripts to identify the elements and perform user actions without any synchronization errors
+
+
+
+
+Design & Architecture:
+
+Design depends on the size of the project but in given project did some separation of selector by making mapping.js file in which user will define the all application input controls selector so if any change occur in future can handle from one file.
+
+
+
+UI/UX: 
+
+Have some issues like on home age ‘Online ticket’ is cutting background image. Moreover, some controls missing ids, classes for automation. Few errors in console.
+For ease in automation define some classes/ids or custom attribute so developers can work on their concerns. Expose some backend API so can validate the data and other aspects better.
+*/
